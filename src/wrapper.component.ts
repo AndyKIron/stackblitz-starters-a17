1d51749c29030a5b370651f99b8884404c57cb0f
AndyKIron <andy.kono@gmail.com>
2023-12-05 11:24:42 +0200

    add fusion-ui angular 17 daterange examplw

--- src/wrapper.component.ts
+++ src/wrapper.component.ts
@@ -1,13 +1,22 @@
 import {Component} from '@angular/core';
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
+import { DaterangeModule } from '@ironsource/fusion-ui/components/daterange/v3';
+import { DaterangeSelection } from '@ironsource/fusion-ui/components/daterange/entities';
+
 
 @Component({
     selector: 'app-story-wrapper',
     standalone: true,
-    imports: [],
+    imports: [ReactiveFormsModule, DaterangeModule],
     template: `
-        <div style="height: 380px; width: 250px;">test</div>`,
+        <div style="height: 380px; width: 250px;">
+        <fusion-daterange
+          [formControl]="formControl"
+        ></fusion-daterange>
+        </div>`,
 })
 
 export class WrapperComponent {
+    formControl: FormControl<DaterangeSelection> = new FormControl();
 
 }
